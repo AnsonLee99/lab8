d75aadab0c51bfa9ac3abca09fff84dc9650e426
Anson <all197@ucsd.edu>
2020-02-26 15:42:53 -0800

    lab8 done

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'VLBElLpQPABfr7Lt0AOGnGsTSnL46fHr';
+
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 13,
+		zoomControl: false
+	});
+
+	// Adding a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
